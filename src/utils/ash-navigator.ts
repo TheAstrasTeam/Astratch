930ba1e0db8d1c4b57d1f20faf06c4bf2cec8b33
KOSHINO <2381068747@qq.com>
2026-08-10 04:03:11 +0800

    fix(ash-navigator): 不支持navigator.userAgentData会导致崩溃

--- src/utils/ash-navigator.ts
+++ src/utils/ash-navigator.ts
@@ -15,29 +15,46 @@ const ALL_PLATFORMS = {
 type TAll_Platforms = (typeof ALL_PLATFORMS)[keyof typeof ALL_PLATFORMS];
 
 const getPlatfrom = (): TAll_Platforms => {
-    // @ts-expect-error 这玩意类型注解不包含
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    const platform = navigator.userAgentData.platform as string;
-    if (platform) {
-        switch (platform) {
-            case 'Windows':
-                return ALL_PLATFORMS.WIN;
-            case 'macOS':
-                return ALL_PLATFORMS.MAC;
-            default:
-                return ALL_PLATFORMS.LINUX;
+    try {
+        // @ts-expect-error 这玩意类型注解不包含
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        const platform = navigator.userAgentData.platform as string;
+        if (platform) {
+            switch (platform) {
+                case 'Windows':
+                    return ALL_PLATFORMS.WIN;
+                case 'macOS':
+                    return ALL_PLATFORMS.MAC;
+                default:
+                    return ALL_PLATFORMS.LINUX;
+            }
+        } else {
+            const navigatorPlatform = navigator.platform;
+            switch (navigatorPlatform) {
+                case 'Win64':
+                case 'Win32':
+                    return ALL_PLATFORMS.WIN;
+                case 'MacIntel':
+                    return ALL_PLATFORMS.MAC;
+                default:
+                    return ALL_PLATFORMS.LINUX;
+            }
         }
-    } else {
-        const navigatorPlatform = navigator.platform;
-        switch (navigatorPlatform) {
-            case 'Win64':
-            case 'Win32':
+    } catch {
+        const platform = navigator.platform;
+        if (platform) {
+            if (platform.startsWith('Win')) {
                 return ALL_PLATFORMS.WIN;
-            case 'MacIntel':
+            }
+            if (platform.startsWith('Mac')) {
                 return ALL_PLATFORMS.MAC;
-            default:
+            }
+            if (platform.startsWith('Linux')) {
                 return ALL_PLATFORMS.LINUX;
+            }
+            return ALL_PLATFORMS.LINUX;
         }
+        return ALL_PLATFORMS.LINUX;
     }
 };
 
