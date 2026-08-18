72330624d7cbd240e914547820a17b2404792a89
LuoTianyi-arm64 <2153585992@qq.com>
2026-08-18 21:03:31 +0800

    Fix: 修复renderer构建脚本异常

--- src/renderer/webgpu/build.sh
+++ src/renderer/webgpu/build.sh
@@ -2,3 +2,4 @@
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen --target web --out-dir ./test --out-name wasm target/wasm32-unknown-unknown/release/webgpu.wasm
 awk 'NR>3' ./test/wasm.d.ts > ./test/wasm.d.ts.tmp && mv ./test/wasm.d.ts.tmp ./test/wasm.d.ts
+awk 'NR>3' ./test/wasm_bg.wasm.d.ts > ./test/wasm_bg.wasm.d.ts.tmp && mv ./test/wasm_bg.wasm.d.ts.tmp ./test/wasm_bg.wasm.d.ts
--- src/renderer/webgpu/test/wasm_bg.wasm.d.ts
+++ src/renderer/webgpu/test/wasm_bg.wasm.d.ts
@@ -1,6 +1,3 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
 export const run: () => void;
 export const __wbindgen_externrefs: WebAssembly.Table;
 export const __wbindgen_start: () => void;
